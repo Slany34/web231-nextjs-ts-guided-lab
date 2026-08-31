1c187713448d3d205c19aef1aaf2678aca55c970
Slany34 <sound2549sound@gmail.com>
2026-08-31 12:23:37 +0700

    components update

---
+++ src/components/CorusesCard.tsx
@@ -0,0 +1,31 @@
+import { Course } from "@/types/course"
+
+// type Course = {
+//         id: number;
+//         code: string;
+//         title: string;
+//         credits: number;
+//         isOpen: boolean;
+// };
+
+// type CourseCardProps = { 
+//   course : Course;
+// };
+type CourseCardProps = { 
+  course: Course; 
+};
+
+export default function CoursesCard({course}: CourseCardProps){
+    return(
+        <>
+                <article key={course.id} className="courseCard">
+                    <h2>{course.title}</h2>
+                    <p>รหัสวิชา: {course.code}</p>
+                    <p>{course.credits} หน่วยกิต</p>
+                    <p>
+                        {course.isOpen ? "เปิดลงทะเบียน" : "ปิดลงทะเบียน"}
+                    </p>
+                </article>
+        </>
+    )
+}
